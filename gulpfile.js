3b889c565b791f3828ac903119e4526ba3bab34e
Aminul Islam <aminmedia@gmail.com>
2016-10-05 18:01:37 +0600

    improved

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,36 @@
-var gulp = require('gulp'),
-	concat  = require('gulp-concat'),
-	uglify 	  = require('gulp-uglify'),
-	cssmin    = require('gulp-cssmin');
+var gulp      = require('gulp'),
+    concat    = require('gulp-concat'),
+    uglify 	  = require('gulp-uglify'),
+    cssnano   = require('gulp-cssnano');
 
 // JS Concat
 gulp.task('js', function() {
-  return gulp.src('./src/js/*.js')
-    .pipe(concat('bundle.js'))
-    .pipe(gulp.dest('./build/js/'));
+  return gulp.src('./src/**/*.js')
+  .pipe(concat('bundle.js'))
+  .pipe(gulp.dest('./build/js/'));
 });
 
 // CSS Concat
 gulp.task('css', function () {
-  return gulp.src('./src/css/*.css')
-    .pipe(concat("css/bundle.css"))
-    .pipe(gulp.dest('build/'));
+  return gulp.src('./src/**/*.css')
+  .pipe(concat("css/bundle.css"))
+  .pipe(gulp.dest('build/'));
 });
 
 // JS Uglify
 gulp.task('uglify', function() {
-  gulp.src('src/js/*.js')
-    .pipe(uglify())
-    .pipe(concat('bundle.min.js'))
-    .pipe(gulp.dest('./build/js/'));
+  gulp.src('src/**/*.js')
+  .pipe(uglify())
+  .pipe(concat('bundle.min.js'))
+  .pipe(gulp.dest('./build/js/'));
 });
 
 // CSS Minify
 gulp.task('minify', function() {
-  gulp.src('src/css/*.css')
-    .pipe(cssmin())
-    .pipe(concat('bundle.min.css'))
-    .pipe(gulp.dest('./build/css/'));
+  gulp.src('src/**/*.css')
+  .pipe(cssnano({autoprefixer: false}))
+  .pipe(concat('bundle.min.css'))
+  .pipe(gulp.dest('./build/css/'));
 });
 
 // Define Global and Default Task
